03c107e0f6a6dd1af7dab6310f6b29259894549f
Sam Lynch <samdawkins1998@gmail.com>
2021-11-06 18:07:27 -0400

    Compelted Assignment

---
+++ index.js
@@ -0,0 +1,37 @@
+function add() {}
+function subtract() {}
+function multiply() {}
+function divide() {}
+
+function add(a, b){
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+
+
+let n = 187;
+function increment (n) {
+    return (++n); 
+}
+
+function decrement (n) {
+    return (-- n);
+}
+
+function makeInt(n) {
+    n = parseInt(n, 10)
+    return n
+
+}
+function preserveDecimal(n) {
+    n = parseFloat(n, 10)
+    return n 
+}
